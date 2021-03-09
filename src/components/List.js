1276e10f17a243ecb68b43e2b0ad1e57b039dcd6
Julio <juliomederosarias@gmail.com>
2021-03-09 11:00:43 -0300

    first commit

---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = () => {
+    return ( 
+        <ul className="list">
+
+        </ul>
+     );
+}
+ 
+export default List;
---
+++ src/components/NewElement.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const NewElement = () => {
+
+    const [element, setElement] = useState('');
+
+    const [list1, setList1] = useState(['Make the bed', 'Eat', 'Walk the dog']);
+
+    const InputElement =(e)=>{
+        setElement(e.target.value);
+    }
+
+    const addElement=(e)=>{
+        e.preventDefault();
+        setList1(list1.concat(element));
+        setElement('');
+        console.log(list1);
+    }
+
+    return ( 
+        <form className="tolist"  onSubmit={addElement} >
+            <input 
+                type="text" 
+                className="new-tolist"
+                placeholder="What needs to be done?"
+                value={element}
+                onChange={InputElement}
+            />
+        </form>
+     );
+}
+ 
+export default NewElement
